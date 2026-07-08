eeea853f5cf2d088cc8ba5b54683722d72e2f5dc
ByteSpace <aayushimohan118@gmail.com>
2026-07-08 15:41:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -40,23 +40,66 @@ app.get('/token', async (req, res) => {
 })
 
 // Socket.io for chat
+const roomUsers = {}
+
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id)
 
-  socket.on('join-room', (room) => {
+  socket.on('join-room', ({ room, username }) => {
     socket.join(room)
-    console.log(`User joined room: ${room}`)
+
+    socket.room = room
+    socket.username = username
+
+    if (!roomUsers[room]) {
+      roomUsers[room] = []
+    }
+
+    roomUsers[room].push(username)
+
+    io.to(room).emit('participant-count', roomUsers[room].length)
+
+    io.to(room).emit('system-message', {
+      text: `${username} joined the meeting`
+    })
+
+    console.log(`${username} joined room ${room}`)
   })
 
   socket.on('send-message', ({ room, message, sender }) => {
-    io.to(room).emit('receive-message', { message, sender })
+    io.to(room).emit('receive-message', {
+      message,
+      sender
+    })
   })
 
   socket.on('disconnect', () => {
+    const room = socket.room
+    const username = socket.username
+
+    if (room && roomUsers[room]) {
+
+      roomUsers[room] =
+        roomUsers[room].filter(
+          user => user !== username
+        )
+
+      io.to(room).emit('participant-count',
+        roomUsers[room].length
+      )
+
+      io.to(room).emit('system-message', {
+        text: `${username} left the meeting`
+      })
+
+      if (roomUsers[room].length === 0) {
+        delete roomUsers[room]
+      }
+    }
+
     console.log('User disconnected:', socket.id)
   })
 })
-
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
